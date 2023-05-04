28d087eb38f3cf603f9380fb7d33269bf4419ca9
AbdullahAli1999 <iifamxx@gmail.com>
2023-05-04 16:10:22 +0300

    lab6!!

--- lab6/script.js
+++ lab6/script.js
@@ -9,9 +9,24 @@ let disLikeElement = document.getElementById("dislikebutton");
 likesElment.innerText = "👍" + intitLikes;
 disLikeElement.innerText = "👎" + initDislikes;
 
+//check for cookies 
+
+window.onload = function(){
+    if(document.cookie != undefined && document.cookie.indexOf('voted')!= -1){
+        disableBtn();
+    }
+}
+
+function setCookie(){
+    document.cookie = "voted=true; Max-Age" + 60;
+
+}
+
 function doLike(){
     upCount++;
     likesElment.innerText = "👍" + upCount
+    disableBtn();
+    setCookie();
     console.log("You Like this!!!!");
 }
 
@@ -19,6 +34,12 @@ function doDislike(){
 
     downCount++;
     disLikeElement.innerText = "👎" + downCount;
-    disLikeElement.disabled = true;
+    disableBtn();
+    setCookie();
     console.log("u DisLike this!");
+}
+
+function disableBtn() {
+    likesElment.disabled = true;
+    disLikeElement.disabled = true;    
 }
